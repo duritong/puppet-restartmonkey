bf5fa97aba862f70edfd68bf3e1c9bbe7b69259e
o <o@immerda.ch>
2015-01-28 22:37:07 +0100

    extend manifest to deploy restart-monkey

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,8 @@
 # manifests/init.pp - module to manage and deploy restart monkey
+
+file{'/usr/local/bin/restart-monkey':
+  source  => "puppet:///modules/restartmonkey/restart_monkey.rb",
+  owner   => root,
+  group   => 0,
+  mode    => '0700';
+}
