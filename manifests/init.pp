373ec2f9e5bbc18d77bf473f255f402d1eafbf79
mh <mh@immerda.ch>
2015-04-26 14:36:52 +0200

    make it more configurable + run it through the night
    additionally we switch to filebased cronjob as we do everywhere.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,43 @@
 # manifests/init.pp - module to manage and deploy restart monkey
-class restartmonkey {
-  file{'/usr/local/sbin/restart-monkey':
-    source  => 'puppet:///modules/restartmonkey/restart_monkey.rb',
-    owner   => root,
-    group   => 0,
-    mode    => '0700';
+class restartmonkey(
+  $active  = true,
+  $dry_run = false,
+  $verbose = false,
+) {
+
+  $dry_run_str = $dry_run ? {
+    true    => ' --dry-run',
+    default => ''
+  }
+  $verbose_str = $verbose ? {
+    true    => ' --verbose',
+    default => ''
   }
 
-  cron { 'puppet_run_restartmonkey':
-    command => '/usr/local/sbin/restart-monkey --dry-run --verbose',
-    user    => 'root',
-    hour    => fqdn_rand(24),
-    minute  => fqdn_rand(59),
-    require => File['/usr/local/sbin/restart-monkey'];
+  $minute_str = fqdn_rand(59)
+  # generate an hour within the night
+  $rand_hour = fqdn_rand(10)
+  $hour_str = (31 - $rand_hour) % 24
+
+  file{
+    '/usr/local/sbin/restart-monkey':
+      source  => 'puppet:///modules/restartmonkey/restart_monkey.rb',
+      owner   => root,
+      group   => 0,
+      mode    => '0700';
+    '/etc/cron.d/run_restartmonkey':
+      require => File['/usr/local/sbin/restart-monkey'];
+  }
+  if $active {
+    File['/etc/cron.d/run_restartmonkey']{
+      content => "${minute_str} ${hour_str} * * * root /usr/local/sbin/restart-monkey${dry_run_str}${verbose_str}\n",
+      owner   => 'root',
+      group   => 0,
+      mode    => '0644',
+    }
+  } else {
+    File['/etc/cron.d/run_restartmonkey']{
+      ensure => 'absent',
+    }
   }
 }
