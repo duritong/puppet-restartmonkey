370003541acfc2a40c2a9cc070bc60436a7a0d00
o <o@immerda.ch>
2015-04-26 14:28:21 +0200

    make cmdline args configurable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 # manifests/init.pp - module to manage and deploy restart monkey
-class restartmonkey {
+class restartmonkey (
+  $cmd_args = ""
+) {
   file{'/usr/local/sbin/restart-monkey':
     source  => 'puppet:///modules/restartmonkey/restart_monkey.rb',
     owner   => root,
@@ -8,7 +10,7 @@ class restartmonkey {
   }
 
   cron { 'puppet_run_restartmonkey':
-    command => '/usr/local/sbin/restart-monkey --dry-run --verbose',
+    command => "/usr/local/sbin/restart-monkey ${cmd_args}",
     user    => 'root',
     hour    => fqdn_rand(24),
     minute  => fqdn_rand(59),
