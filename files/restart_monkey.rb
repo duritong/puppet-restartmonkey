711e44b94e0d13a511984dbe8c072be92bfb1197
mh <mh@immerda.ch>
2021-06-16 22:19:51 +0200

    don't try to restart auditd also on EL8

--- files/restart_monkey.rb
+++ files/restart_monkey.rb
@@ -296,6 +296,7 @@ class Cnf
 
     @cnf['must_reboot'] ||= {}
     default_must_reboot = {
+      'CentOS.8' => ['auditd', 'dbus'],
       'CentOS.7' => ['auditd', 'dbus'],
       'CentOS.6' => ['udev-post','getty-reboot'],
       'CentOS.5' => ['xend-reboot'],
